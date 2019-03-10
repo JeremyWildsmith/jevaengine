14211cdf920d74470a403a8d9700836e9c1a64ca
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2019-03-10 12:54:00 -0700

    Added null input source

---
+++ src/main/java/io/github/jevaengine/joystick/NullInputSource.java
@@ -0,0 +1,8 @@
+package io.github.jevaengine.joystick;
+
+public class NullInputSource implements IInputSource {
+    @Override
+    public void process(IInputSourceProcessor processor) {
+
+    }
+}
